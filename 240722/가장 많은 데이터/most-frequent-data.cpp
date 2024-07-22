e0d0652076d5b9f4b7977d0d35b2863d5a4fc867
dh4m <107628935+dh4m@users.noreply.github.com>
2024-07-22 10:12:25 +0900

    Title: 가장 많은 데이터, Time: 183ms, Memory: 9MB, Status: Passed - CodeTree

--- 240722/가장 많은 데이터/most-frequent-data.cpp
+++ 240722/가장 많은 데이터/most-frequent-data.cpp
@@ -7,21 +7,16 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
+    int ans = 0;
 
     unordered_map<string, int> cnt_str;
     for (int i = 0; i < n; i++) {
         string str;
         cin >> str;
-        for (int i = 0; i < str.length(); i++) {
-            if ('A' <= str[i] && str[i] <= 'Z') {
-                str[i] -= 'A' - 'a';
-            }
-        }
         cnt_str[str]++;
+        if (cnt_str[str] > ans)
+            ans = cnt_str[str];
     }
-    cout << max_element(cnt_str.begin(), cnt_str.end(), 
-            [](const pair<string, int>& a, const pair<string, int>& b) {
-                return a.second < b.second;
-            })->second;
+    cout << ans << '\n';
     return 0;
 }
