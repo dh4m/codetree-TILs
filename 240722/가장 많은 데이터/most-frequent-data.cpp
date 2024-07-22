a56ca9da066485bccd0b024655aa079edc4baf8d
dh4m <107628935+dh4m@users.noreply.github.com>
2024-07-22 09:40:17 +0900

    Title: 가장 많은 데이터, Time: 110ms, Memory: 7MB, Status: Wrong Answer - CodeTree

--- 240722/가장 많은 데이터/most-frequent-data.cpp
+++ 240722/가장 많은 데이터/most-frequent-data.cpp
@@ -12,6 +12,11 @@ int main() {
     for (int i = 0; i < n; i++) {
         string str;
         cin >> str;
+        for (auto it: str) {
+            if ('A' <= it && it <= 'Z') {
+                it -= 'A' - 'a';
+            }
+        }
         cnt_str[str]++;
     }
     cout << max_element(cnt_str.begin(), cnt_str.end())->second;
