07b6853dcdd576b680cde6732d722659286e6370
dh4m <107628935+dh4m@users.noreply.github.com>
2024-07-22 13:25:48 +0900

    Title: 세 수의 합, Time: 246ms, Memory: 20MB, Status: Passed - CodeTree

--- 240722/세 수의 합/sum-of-three-num.cpp
+++ 240722/세 수의 합/sum-of-three-num.cpp
@@ -9,18 +9,20 @@ int main() {
     int n, k;
     cin >> n >> k;
     unordered_map<int, int> cnt_int;
-    unordered_map<int, int> cnt_sum;
+    int arr[1000];
     int num;
     int res = 0;
-    cin >> num;
-    cnt_int[num]++;
-    for (int i = 1; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> num;
-        res += cnt_sum[k - num];
-        for (auto it: cnt_int) {
-            cnt_sum[num + it.first] += it.second;
-        }
+        arr[i] = num;
         cnt_int[num]++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cnt_int[arr[i]]--;
+        for (int j = 0; j < i; j++) {
+            res += cnt_int[k - arr[i] - arr[j]];
+        }
     }
     cout << res << '\n';
     return 0;
