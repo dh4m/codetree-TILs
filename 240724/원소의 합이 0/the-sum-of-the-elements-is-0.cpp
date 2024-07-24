8c21141280d47ceca14052332b9b3422d4f83924
dh4m <107628935+dh4m@users.noreply.github.com>
2024-07-24 10:10:11 +0900

    Title: 원소의 합이 0, Time: 901ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 240724/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
+++ 240724/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -6,21 +6,25 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n;
     cin >> n;
-    unordered_map<int, int> seq[4];
+    unordered_map<int, int> seq[2];
+    int write_map = 0;
     for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
         seq[0][num]++;
     }
+    write_map = 1;
     for (int s = 1; s < 4; s++) {
+        seq[write_map].clear();
         for (int i = 0; i < n; i++) {
             int num;
             cin >> num;
-            for (auto it: seq[s - 1]) {
-                seq[s][num + it.first] += it.second;
+            for (auto it: seq[!write_map]) {
+                seq[write_map][num + it.first] += it.second;
             }
         }
+        write_map = !write_map;
     }
-    cout << seq[3][0];
+    cout << seq[!write_map][0];
     return 0;
 }
