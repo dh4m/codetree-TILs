230246c28c20796e1d01a125acb6cd243d8054f8
dh4m <107628935+dh4m@users.noreply.github.com>
2024-07-25 08:45:16 +0900

    Title: 단어장, Time: 60ms, Memory: 2MB, Status: Passed - CodeTree

---
+++ 240725/단어장/word-list.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+    map<string, int> word_cnt;
+    for (int i = 0; i < n; i++) {
+        string str;
+        cin >> str;
+        word_cnt[str]++;
+    }
+    for (const auto &it: word_cnt) {
+        cout << it.first << ' ' << it.second << '\n';
+    }
+    return 0;
+}
