638c05b92efa7cf3dc2e06ef84a05d66c144485a
dh4m <107628935+dh4m@users.noreply.github.com>
2024-07-24 13:48:33 +0900

    Title: 원소의 합이 0, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240724/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
+++ 240724/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -6,35 +6,36 @@ int main() {
     ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int n;
     cin >> n;
-    unordered_map<int, int> seq[2];
-    int write_map = 0;
+    unordered_map<int, int> seq[4];
     for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
-        seq[0][num]++;
+        seq[0][num] ++;
     }
-    write_map = 1;
-    for (int s = 1; s < 3; s++) {
-        seq[write_map].clear();
-        for (int i = 0; i < n; i++) {
-            int num;
-            cin >> num;
-            for (auto it: seq[!write_map]) {
-                seq[write_map][num + it.first] += it.second;
-            }
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        for (const auto &it: seq[0]) {
+            seq[1][num + it.first] += it.second;
         }
-        write_map = !write_map;
     }
-
-    int res = 0;
-    seq[write_map].clear();
     for (int i = 0; i < n; i++) {
         int num;
         cin >> num;
-        for (auto it: seq[!write_map]) {
-            if (num + it.first == 0) {
-                res += it.second;
-            }
+        seq[2][num] ++;
+    }
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        for (const auto &it: seq[2]) {
+            seq[3][num + it.first] += it.second;
+        }
+    }
+    int res = 0;
+    for (const auto &i1: seq[1]) {
+        for (const auto &i2: seq[3]) {
+            if (i1.first + i2.first == 0)
+                res += i2.second;
         }
     }
     cout << res;
