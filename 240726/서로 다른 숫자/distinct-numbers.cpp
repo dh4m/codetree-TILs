c22dd037508c9eafe8ae8ac18dd57cd55159f536
dh4m <107628935+dh4m@users.noreply.github.com>
2024-07-26 12:44:14 +0900

    Title: 서로 다른 숫자, Time: 35ms, Memory: 2MB, Status: Passed - CodeTree

---
+++ 240726/서로 다른 숫자/distinct-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    int n;
+    unordered_set<int> s;
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        s.insert(num);
+    }
+    int cnt = 0;
+    for (const auto &it : s) {
+        cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
